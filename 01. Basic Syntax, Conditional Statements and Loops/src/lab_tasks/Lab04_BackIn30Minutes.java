170b6dfe138027765361302cf4914cd62fa01e04
Ivan Petkov <ivan.petkov@externals.adidas.com>
2022-01-05 22:02:29 +0200

    Basics Recap - solve all lab tasks

---
+++ 01. Basic Syntax, Conditional Statements and Loops/src/lab_tasks/Lab04_BackIn30Minutes.java
@@ -0,0 +1,27 @@
+package lab_tasks;
+
+import java.util.Scanner;
+
+public class Lab04_BackIn30Minutes {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int hours = Integer.parseInt(scanner.nextLine());
+        int minutes = Integer.parseInt(scanner.nextLine());
+
+        minutes += 30;
+        if (minutes > 59) {
+            hours++;
+            minutes-=60;
+        }
+        if (hours > 23) {
+            hours = 0;
+        }
+
+        if (minutes < 10) {
+            System.out.printf("%d:0%d%n", hours, minutes);
+        } else {
+            System.out.printf("%d:%d", hours, minutes);
+        }
+    }
+}
